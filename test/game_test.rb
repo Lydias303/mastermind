05717702cf3e7abd1f24b4e69232d8df9369a865
Lydias303 <shaferlydia@yahoo.com>
2014-11-13 14:49:20 -0700

    fixed game test, refactored game.rb

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,14 +6,14 @@ require 'validate_guess'
 class GameTest < Minitest::Test
 
   def test_you_can_win
-    solution = ['r','r','r','r']
-    guess = ValidateGuess.new(['r','r','r','r'])
-    assert true, solution == guess
+    solution  = ['r','r','r','r']
+    validator = ValidateGuess.new(['r','r','r','r'])
+    assert solution == validator.guess
   end
 
   def test_you_can_lose
-    solution = ['r','r','r','r']
-    guess = ValidateGuess.new(['r','b','b','r'])
-    assert true, solution != guess
+    solution  = ['r','r','r','r']
+    validator = ValidateGuess.new(['r','b','b','r'])
+    assert solution != validator.guess
   end
 end
